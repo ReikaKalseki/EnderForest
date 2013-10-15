c5e90f4d92346f14fa6befae7e552f6a3d9f2b59
Reika <reikasminecraft@gmail.com>
2013-10-15 13:59:05 -0400

    Creation

---
+++ EnderForest.java
@@ -0,0 +1,83 @@
+package Reika.EnderForest;
+
+import java.net.URL;
+
+import Reika.DragonAPI.Base.DragonAPIMod;
+import Reika.DragonAPI.Instantiable.ModLogger;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class EnderForest extends DragonAPIMod {
+
+	@Override
+	@EventHandler
+	public void preload(FMLPreInitializationEvent evt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	@EventHandler
+	public void load(FMLInitializationEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	@EventHandler
+	public void postload(FMLPostInitializationEvent evt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getDisplayName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getModAuthorName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URL getDocumentationSite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasWiki() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public URL getWiki() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasVersion() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getVersionName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModLogger getModLogger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
