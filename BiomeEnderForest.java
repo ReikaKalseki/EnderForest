89da692f78939cc900559421b4b49413ac9806d6
Reika <reikasminecraft@gmail.com>
2013-10-15 15:48:21 -0400

    Update

---
+++ BiomeEnderForest.java
@@ -0,0 +1,11 @@
+package Reika.EnderForest;
+
+import net.minecraft.world.biome.BiomeGenForest;
+
+public class BiomeEnderForest extends BiomeGenForest {
+
+	public BiomeEnderForest(int id) {
+		super(id);
+	}
+
+}
--- EnderForest.java
+++ EnderForest.java
@@ -14,69 +14,58 @@ public class EnderForest extends DragonAPIMod {
 	@Override
 	@EventHandler
 	public void preload(FMLPreInitializationEvent evt) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	@EventHandler
 	public void postload(FMLPostInitializationEvent evt) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public String getDisplayName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getModAuthorName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public URL getDocumentationSite() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean hasWiki() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public URL getWiki() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean hasVersion() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public String getVersionName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ModLogger getModLogger() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
