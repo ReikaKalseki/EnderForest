57680a06e5f7c5ba151e8667338a319dc68e009f
Reika <reikasminecraft@gmail.com>
2013-10-15 23:32:32 -0400

    Loadable

--- BiomeEnderForest.java
+++ BiomeEnderForest.java
@@ -1,11 +1,28 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2013
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
 package Reika.EnderForest;
 
+import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.world.biome.BiomeGenForest;
+import net.minecraft.world.biome.SpawnListEntry;
+import Reika.DragonAPI.Auxiliary.ModList;
 
 public class BiomeEnderForest extends BiomeGenForest {
 
 	public BiomeEnderForest(int id) {
 		super(id);
+		spawnableMonsterList.add(new SpawnListEntry(EntityEnderman.class, 10, 1, 4)); //boost Enderman spawn rates
+
+		if (ModList.THERMALEXPANSION.isLoaded()) {
+			//resonant ender pools
+		}
 	}
 
 }
--- EnderForest.java
+++ EnderForest.java
@@ -1,26 +1,45 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2013
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
 package Reika.EnderForest;
 
 import java.net.URL;
 
+import Reika.DragonAPI.DragonAPICore;
 import Reika.DragonAPI.Base.DragonAPIMod;
 import Reika.DragonAPI.Instantiable.ModLogger;
+import Reika.DragonAPI.Libraries.World.ReikaBiomeHelper;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
 
+@Mod(modid = "EnderForest", name="Ender Forest", version="beta", certificateFingerprint = "@GET_FINGERPRINT@", dependencies="after:DragonAPI")
+@NetworkMod(clientSideRequired = true, serverSideRequired = true)
 public class EnderForest extends DragonAPIMod {
+
+	public static EnderForest instance = new EnderForest();
+	public static ModLogger logger;
 
 	@Override
 	@EventHandler
 	public void preload(FMLPreInitializationEvent evt) {
-
+		logger = new ModLogger(instance, true, false, false);
 	}
 
 	@Override
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
-
+		GameRegistry.addBiome(new BiomeEnderForest(ReikaBiomeHelper.getFirstEmptyBiomeIndex()));
 	}
 
 	@Override
@@ -31,17 +50,17 @@ public class EnderForest extends DragonAPIMod {
 
 	@Override
 	public String getDisplayName() {
-		return null;
+		return "Ender Forest";
 	}
 
 	@Override
 	public String getModAuthorName() {
-		return null;
+		return "Reika";
 	}
 
 	@Override
 	public URL getDocumentationSite() {
-		return null;
+		return DragonAPICore.getReikaForumPage(instance);
 	}
 
 	@Override
@@ -66,7 +85,7 @@ public class EnderForest extends DragonAPIMod {
 
 	@Override
 	public ModLogger getModLogger() {
-		return null;
+		return logger;
 	}
 
 }
